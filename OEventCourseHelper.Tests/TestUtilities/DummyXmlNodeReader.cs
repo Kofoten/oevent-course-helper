95b713f09cc6231e1fd44172884e4169c7c3b344
Rasmus Steen <rasmus@steen.ninja>
2026-03-25 23:05:18 +0100

    adding tests

---
+++ OEventCourseHelper.Tests/TestUtilities/DummyXmlNodeReader.cs
@@ -0,0 +1,16 @@
+﻿using OEventCourseHelper.Xml;
+
+using System.Xml;
+
+/// <summary>
+/// A minimal reader that does nothing but satisfy the interface, 
+/// allowing IOFXmlReader to perform its schema validation.
+/// </summary>
+internal class DummyXmlNodeReader : IXmlNodeReader
+{
+    public Action<string>? OnValidationError { get; set; }
+
+    public bool CanRead(XmlReader reader) => false;
+
+    public void Read(XmlReader reader) { }
+}
